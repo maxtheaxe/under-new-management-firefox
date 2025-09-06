64d3989e4cecd08a6476d8a02df9de3b1f15c73a
Chloe <chloe@sapphic.moe>
2025-09-06 22:07:40 +0500

    switch to happy-dom, jsdom does not play nice at all

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ import { WxtVitest } from "wxt/testing";
 export default defineConfig({
     plugins: [WxtVitest()],
     test: {
-        environment: "jsdom",
+        environment: "happy-dom",
         setupFiles: "./vitest.setup.ts",
     }
 });
